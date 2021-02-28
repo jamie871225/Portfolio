3fe0ffe293fa824575cf9e6a7479ad3787cdfa87
jamie871225 <75563148+jamie871225@users.noreply.github.com>
2021-02-28 17:45:01 +0800

    Delete HexSchool/Hamburger directory

--- HexSchool/Hamburger/js/all.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(event){
-    $(".top a").click(function (event) {
-        event.preventDefault();
-        $("html,body").animate({
-          scrollTop: 0
-        }, 700);
-      });
-});
