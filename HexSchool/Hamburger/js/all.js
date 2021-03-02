41f6997e395c33c35bfbeefe78662680e86b034d
jamie871225 <75563148+jamie871225@users.noreply.github.com>
2021-03-02 19:20:50 +0800

    Add files via upload

---
+++ HexSchool/Hamburger/js/all.js
@@ -0,0 +1,21 @@
+$(document).ready(function(event){
+    $(".top a").click(function(event) {
+        event.preventDefault();
+        $("html,body").animate({
+          scrollTop: 0
+        }, 700);
+      });
+    $(".header-wrap .icon-nav").click(function(event){
+      event.preventDefault();
+      $(this).parent().parent().parent().parent().find(".cart .menu ul").slideUp();
+      $(".header-wrap .nav").slideToggle();
+    });
+    $(".cart .menu .icon-menu").click(function(event){
+      event.preventDefault();
+      $(this).parent().parent().parent().parent().parent().find("header .header-wrap ul").slideUp();
+      $(".cart .menu ul").slideToggle();
+    });
+    $(".hollow-heart").click(function(event){
+      $(this).parent().find(".solid-heart").toggleClass("like");
+    });
+});
